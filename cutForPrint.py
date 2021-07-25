e52a9148edad5879b4e73c968142f25b3ca5416f
Nikita <grafniksan@gmail.com>
2021-07-25 20:27:06 +0300

    Progress fix & cutter update

--- cutForPrint.py
+++ cutForPrint.py
@@ -3,17 +3,20 @@ from PIL import Image
 
 PIL.Image.MAX_IMAGE_PIXELS = 12331200000
 
-im = PIL.Image.open("resultmap.png")
-imgwidth, imgheight = im.size
 
-w, h = 6, 6 # how many parts
-iw = imgwidth // w
-ih = imgheight // h
+def cut(im, w, h, outputFolder="png"):
+    imgwidth, imgheight = im.size
+    iw = imgwidth // w
+    ih = imgheight // h
+    c = 0
+    for i in range(h):
+        for j in range(w):
+            area = (iw * j, ih * i, iw * (j + 1), ih * (i + 1))
+            o = im.crop(area)
+            o.save(f"{outputFolder}/{c:03}.png")
+            c += 1
 
-c = 0
-for i in range(h):
-    for j in range(w):
-        area = (iw * j, ih * i, iw *( j +1 ), ih *( i +1))
-        o = im.crop(area)
-        o.save(f"png/{c:03}.png")
-        c +=1
+
+if __name__ == "__main__":
+    im = PIL.Image.open("resultmap.png")
+    cut(im, 6, 5)
--- pythonUnversalMapCollector.py
+++ pythonUnversalMapCollector.py
@@ -4,11 +4,12 @@ from PIL import Image
 from io import BytesIO
 from tqdm import tqdm
 from selenium.webdriver.chrome.options import Options
+import cutForPrint
 
-INIT_LINK = "https://yandex.ru/maps/213/moscow/?ll=37.624027%2C55.753747&z=15.68"
+INIT_LINK = "https://yandex.ru/maps/213/moscow/?ll=37.622504%2C55.753215&z=13"
 
-SCREENSHOOT_WIDTH = 30000
-SCREENSHOOT_HEIGHT = 45000
+SCREENSHOOT_WIDTH = 14472
+SCREENSHOOT_HEIGHT = 17060
 
 SCREENSHOOT_self_boundSize = 500  # Different services have different safe zones
 
@@ -50,16 +51,16 @@ class MapCollector:
         distanceToStartX = self.width // 2 - self.boundSizeHalf
         distanceToStartY = self.height // 2 - self.boundSizeHalf
 
-        pbar = tqdm(desc="MoveToStart", total=distanceToStartX + distanceToStartY)
-        while 1:
-            toMoveX = min(distanceToStartX, toMoveX)
-            toMoveY = min(distanceToStartY, toMoveY)
-            self.slideToOffset(toMoveX, toMoveY)
-            distanceToStartX -= toMoveX
-            distanceToStartY -= toMoveY
-            pbar.update(toMoveX + toMoveY)
-            if distanceToStartX == 0 and distanceToStartY == 0:
-                break
+        with tqdm(desc="MoveToStart", total=distanceToStartX + distanceToStartY) as pbar:
+            while 1:
+                toMoveX = min(distanceToStartX, toMoveX)
+                toMoveY = min(distanceToStartY, toMoveY)
+                self.slideToOffset(toMoveX, toMoveY)
+                distanceToStartX -= toMoveX
+                distanceToStartY -= toMoveY
+                pbar.update(toMoveX + toMoveY)
+                if distanceToStartX == 0 and distanceToStartY == 0:
+                    break
         print("At the start position")
 
     def slideToOffset(self, x, y):
@@ -82,20 +83,20 @@ class MapCollector:
 
         finalScreenShoot = Image.new('RGB', (width, height))
         sign = 1
-        reverse = width - width % boundSize - boundSize
-        pbar = tqdm(desc="CollectingMap", total=((height // boundSize + (1 if height % boundSize > 0 else 0)) * (
-                width // boundSize + (1 if width % boundSize > 0 else 0))))
         widminusbs = width - boundSize
-        for y in range(0, height, boundSize):
-            for x in range(0, width, boundSize):
-                png = browser.get_screenshot_as_png()
-                im = Image.open(BytesIO(png))
-                finalScreenShoot.paste(smallScreenShoot(im), (x if sign > 0 else reverse - x, y))
-                if widminusbs - x > 0:
-                    self.slideToOffset(-boundSize * sign, 0)
-                pbar.update(1)
-            sign *= -1
-            self.slideToOffset(0, -boundSize)
+        reverse = width - width % boundSize - boundSize
+        with tqdm(desc="CollectingMap", total=((height // boundSize + (1 if height % boundSize > 0 else 0)) * (
+                width // boundSize + (1 if width % boundSize > 0 else 0)))) as pbar:
+            for y in range(0, height, boundSize):
+                for x in range(0, width, boundSize):
+                    png = browser.get_screenshot_as_png()
+                    im = Image.open(BytesIO(png))
+                    finalScreenShoot.paste(smallScreenShoot(im), (x if sign > 0 else reverse - x, y))
+                    if widminusbs - x > 0:
+                        self.slideToOffset(-boundSize * sign, 0)
+                    pbar.update(1)
+                sign *= -1
+                self.slideToOffset(0, -boundSize)
         return finalScreenShoot
 
 
@@ -103,4 +104,5 @@ collector = MapCollector(INIT_LINK, SCREENSHOOT_WIDTH, SCREENSHOOT_HEIGHT, SCREE
                          pointIsCenter=True, sidebar=SIDEBAR)
 shoot = collector.finalShoot()
 shoot.save("resultmap.png")
+cutForPrint.cut(shoot, 6, 5)
 # shoot.show()
